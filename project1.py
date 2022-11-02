5fbf060fe5deae7615b609ee0c0924b2877f4357
LuiM112 <89751770+LuiM112@users.noreply.github.com>
2022-11-01 19:21:36 -0700

    update
    Question 2 completed?

--- project1.py
+++ project1.py
@@ -18,7 +18,7 @@ df = pd.read_csv("https://raw.githubusercontent.com/GarrettShrop/Sonoma-Animal-S
 
 df.info()
 
-df['Type'].value_counts().plot.bar()
+#df['Type'].value_counts().plot.bar();
 
 df.isna().sum()
 
@@ -33,7 +33,7 @@ df_cat['Outcome Type'].value_counts()
 df_dog['Outcome Type'].value_counts()
 
 df_dog_cat = df[df['Type'].str.contains('DOG|CAT')]
-df_dog_cat['Type'].value_counts().plot.bar()
+#df_dog_cat['Type'].value_counts().plot.bar();
 
 rto = ['RETURN TO OWNER','RTOS']
 
@@ -43,5 +43,23 @@ df_dog_cat['Outcome Type'].replace(rto,'RTO', inplace=True)
 
 dead = ['EUTHANIZE', 'DISPOSAL','DIED']
 df_dog_cat['Outcome Type'].replace(dead,'DEAD', inplace=True)
+df_dog_cat = df_dog_cat[df['Outcome Type'] != 'ESCAPED/STOLEN']
 df_dog_cat['Outcome Type'].value_counts()
-sns.countplot(data=df_dog_cat, x='Outcome Type', hue='Type')
+sns.countplot(data=df_dog_cat, x='Outcome Type', hue='Type');
+
+# 2) if a animal has a name what is the different intake type?
+df_name = df.copy()
+df['Name'].isna().mean()
+((df['Intake Type'] == 'STRAY') & (df['Name'].notna())).mean()
+df_name = df_name.drop(columns=['Count','Location'])
+df_name['Name'].fillna('Nameless', inplace=True)
+
+def named(name):
+  if pd.isna(name):
+    return "Nameless"
+  else:
+    return "Named"
+
+df_name['name_status'] = df['Name'].apply(named)
+df_name.groupby('name_status')['Intake Type'].value_counts().unstack(0).plot.barh()
+df_name['Intake Type'].value_counts()
